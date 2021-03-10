814865f2155719c6663035bbe655810ad8b738e9
Terje Sylvarnes <fuzzklang@gmail.com>
2021-03-10 19:16:33 +0100

    Add RSS-item data class

--- app/src/test/java/com/example/team_23/API.kt
+++ app/src/test/java/com/example/team_23/API.kt
@@ -13,7 +13,9 @@ class API {
     private val params = "event=forestFire&show=all"
     val period = "period=2019-05"
 
-    fun fetchData() {
+    // Fetches all alerts from MetAlerts
+    // Each item in feed contains URL to specific alert
+    fun fetchAllAlerts() {
         /*CoroutineScope(Dispatchers.IO).launch {
             val APITAG = "API fetching: "
             try {
@@ -25,16 +27,31 @@ class API {
             }
         }*/
     }
+
+    // Fetch information on specific alert, and create Alert-object
+    fun fetchAlert(url: String): Alert {
+        return Alert("")
+    }
 }
+
+// Data class for items in RSS-feed fetched from MetAlerts
+// Each item directs to an alert (via link)
+data class RssItem (
+    val title: String,
+    val description: String,
+    val link: String
+)
 
 // Source: https://api.met.no/weatherapi/metalerts/1.1/documentation#DESCRIPTION
 // Corresponds to Alert described in MET report
-// contains selected fields from CAP-alert
+// Contains only selected fields from CAP-alert
+// Need the HTTP-requests to properly work to be able to test these
 data class Alert (
     val identifier: String, // Unique identifier issued by MET or NVE
-    val sent: String,       // Time of sending
-    val status: String,     // 'Actual' or 'Test' (should be ignored)
-    val msgType: String,    // 'Alert': first message. 'Update': replaces previous messages. 'Cancel': cancel previous erroneous message, or danger ended.
+    //val sent: String,       // Time of sending
+    //val status: String,     // 'Actual' or 'Test' (should be ignored)
+    //val msgType: String,    // 'Alert': first message. 'Update': replaces previous messages. 'Cancel': cancel previous erroneous message, or danger ended.
+    //val info: Info
 )
 
 // Contains more detailed information on an Alert
@@ -42,18 +59,18 @@ data class Info (
     val event: String,        // In our project only 'forestFire' relevant
     val responseType: String, // 'Monitor': act according to <instruction>. 'AllClear': danger ended.
     // val urgency: String,   // 'Future', 'Immediate'. Unsure if actually used by MetAlerts
-    val severity: String,     // 'Extreme', 'Severe', 'Moderate', 'Minor'.
-    val certainty: String,    // 'Likely', 'Possible', 'Observed'.
-    val effective: String,    // Message is valid from this time
-    val onset: String,        // Starting time of event
-    val expires: String,      // Message is valid until this time
-    val instruction: String   // Description of recommended action
+    //val severity: String,     // 'Extreme', 'Severe', 'Moderate', 'Minor'.
+    //val certainty: String,    // 'Likely', 'Possible', 'Observed'.
+    //val effective: String,    // Message is valid from this time
+    //val onset: String,        // Starting time of event
+    //val expires: String,      // Message is valid until this time
+    //val instruction: String   // Description of recommended action
     // + much more
+    //val area: Area
 )
 
 // Area which alert is valid for
 data class Area (
-    val area: String,     // Box enclosing geographical description (?)
     val areaDesc: String, // Description of geographical area for alert
     val polygon: String   // Polygon encircling the current area
 )
