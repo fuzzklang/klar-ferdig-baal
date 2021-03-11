a2caccb4d34931d44895f9dcefbf9e6c1f7784fc
Terje Sylvarnes <fuzzklang@gmail.com>
2021-03-11 18:59:04 +0100

    Fix wrongly formatted XML test-string. Prepare testing code.

--- app/src/androidTest/java/com/example/team_23/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/team_23/ExampleInstrumentedTest.kt
@@ -3,15 +3,11 @@ package com.example.team_23
 import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.github.kittinunf.fuel.Fuel
-import com.github.kittinunf.fuel.core.Headers
-import com.github.kittinunf.fuel.coroutines.awaitString
-import kotlinx.coroutines.runBlocking
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import java.io.InputStream
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -35,30 +31,41 @@ class ExampleInstrumentedTest {
 
         val endpoint = "https://api.met.no/weatherapi/metalerts/1.1/"
         val eventType = "event=forestFire"
-        val period = "period=2019-05"
+        val period = "2019-05"
         val otherParams = "show=all"
         val url = "$endpoint?$eventType&$period"
 
-        runBlocking {
+        testParseRSS(rssFeedString)  // Simulated API-call
+
+        /*runBlocking {
             try {
                 Log.d(tag, "URL: $url")
                 // Must add non-generic User-Agent to Get-request Header to access MetAlerts API
                 // as described in Terms of Service. Not necessary towards IN2000-proxy
+                //val httpResponse = Fuel.get(url).awaitString()
+
                 val httpResponse = Fuel.get(url)
                         .header(Headers.USER_AGENT, "Team23-IN2000_IFI_V2021 fuzzklang@gmail.com").awaitString()
-                //val httpResponse = Fuel.get(url).awaitString()
                 Log.d(tag, httpResponse)
                 testParseRSS(httpResponse)
             } catch (exception: Exception) {
                 Log.d(tag,"A network request exception was thrown: ${exception.message}")
             }
-        }
-
+        }*/
     }
 
     fun testParseRSS(rssFeed: String) {
         val tag = "testParseRSS"
-        Log.d(tag,"FEED:\n\n$rssFeed")
+        Log.d(tag, "running testParseRSS")
+        //val inputStream: InputStream = rssFeed.byteInputStream()  // Not working?
+        val inputStream: InputStream = rssFeed.byteInputStream()
+        // Parses RSS feed and returns list of RssItem
+        MetAlertsRssParser().parse(inputStream)
+
+        /*val rssItems: List<RssItem> = MetAlertsRssXmlParser().parse(inputStream) as List<RssItem>
+        for (i in rssItems) {
+            Log.d(tag, "RSS-Item: $i")
+        }*/
     }
 }
 
@@ -129,8 +136,29 @@ val rssFeedString = """
           <guid>2.49.0.1.578.0.190517064153070.1904</guid>
           <pubDate>Fri, 17 May 2019 06:41:53 +0000</pubDate>
         </item>
-        <item>
-          <title>Skogbrannfare, gult niv&#xE5;, Hordaland, 16 May 14:00 UTC til 18 May 04:00 UTC.</title>
-          <description>Alert: Det er lokal skogbrannfare inntil det kommer nedb&#xF8;r av betydning.</description>
-          <link>https://api.met.no/weatherapi/metalerts/1.1?cap=2.49.0.1.578.0.190516150827657.1901&amp
-""".trimIndent()
+""".trimIndent().trimStart()
+
+
+// RSS feed with no alerts
+val rssFeedStringNoAlert = """
+    <?xml version="1.0"?>
+    <rss version="2.0">
+      <channel>
+        <title>MET farevarsel</title>
+        <link>https://api.met.no</link>
+        <description>Farevarsler fra Meteorologisk institutt</description>
+        <language>no</language>
+        <copyright>Copyright The Norwegian Meteorological Institute, licensed under Norwegian license for public data (NLOD) and Creative Commons 4.0 BY</copyright>
+        <pubDate>Thu, 11 Mar 2021 15:45:37 +0000</pubDate>
+        <lastBuildDate>Thu, 11 Mar 2021 15:45:37 +0000</lastBuildDate>
+        <category>Met</category>
+        <generator>XML::LibXML::Generator 0.1</generator>
+        <docs>http://blogs.law.harvard.edu/tech/rss</docs>
+        <image>
+          <title>MET farevarsel</title>
+          <link>https://api.met.no</link>
+          <url>https://api.met.no/images/logo_2013_no.png</url>
+        </image>
+      </channel>
+    </rss>
+""".trimIndent().trimStart()
