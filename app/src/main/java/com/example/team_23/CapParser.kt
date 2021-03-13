b55bde8b983997685b5a4242d73e2bfc8f87cf6c
Terje Sylvarnes <fuzzklang@gmail.com>
2021-03-13 14:20:11 +0100

    Add parse-function to Cap Parser.

--- app/src/main/java/com/example/team_23/CapParser.kt
+++ app/src/main/java/com/example/team_23/CapParser.kt
@@ -1,5 +1,26 @@
 package com.example.team_23
 
-class CapParser {
+import android.util.Log
+import android.util.Xml
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserException
+import java.io.IOException
+import java.io.InputStream
 
+class CapParser {
+    @Throws(XmlPullParserException::class, IOException::class)
+    fun parse(inputStream: InputStream): Alert {
+        val tag = "RssParser.parse"
+        inputStream.use { inputStream ->
+            val parser: XmlPullParser = Xml.newPullParser()
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false)
+            parser.setInput(inputStream, null)
+            parser.nextTag()
+            Log.d(tag, "Current XML-tag: ${parser.name}")
+            return Alert("identifier")
+            //return readCap(parser)
+        }
+    }
+
+    // TODO: Complete parsing of CAP alerts
 }
