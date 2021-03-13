10a6ba4069b2b8d9acce53034bef23953d052cf1
Terje Sylvarnes <fuzzklang@gmail.com>
2021-03-13 14:57:11 +0100

    Make minimal functional app (show alert/info as text)

--- app/src/main/java/com/example/team_23/MainActivity.kt
+++ app/src/main/java/com/example/team_23/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.team_23
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.myTextView)
+        textView.text = "RESET TEXT"
+
+        val titleView = findViewById<TextView>(R.id.titleView)
+        titleView.text = "Alert fra mai 2019"
+
+        // Read from local XML test file
+        // Show info (text) on screen
+        val rssFeed = assets.open("rssFeedWithAlerts.xml")
+        val info = MetAlertsRssParser().parse(rssFeed)
+        val infoString = info.joinToString (separator = "\n\n") { it.toString() }
+        textView.text = infoString
     }
 }
