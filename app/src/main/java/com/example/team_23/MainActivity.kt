aa8d31f8b305d0cbfc6b5a14cb7d92b186f948f5
Terje Sylvarnes <fuzzklang@gmail.com>
2021-03-15 13:35:43 +0100

    Add API-calls to API-class.

--- app/src/main/java/com/example/team_23/MainActivity.kt
+++ app/src/main/java/com/example/team_23/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.team_23
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import com.example.team_23.api.CapParser
 import com.example.team_23.api.MetAlertsRssParser
+import com.example.team_23.api.API
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tag = "MainActivity"
 
         val textView = findViewById<TextView>(R.id.myTextView)
         textView.text = "RESET TEXT"
@@ -16,11 +21,24 @@ class MainActivity : AppCompatActivity() {
         val titleView = findViewById<TextView>(R.id.titleView)
         titleView.text = "Alert fra mai 2019"
 
+        Log.d(tag, "Calling API")
+        API().fetchAllAlerts()
+
         // Read from local XML test file
         // Show info (text) on screen
-        val rssFeed = assets.open("rssFeedWithAlerts.xml")
-        val info = MetAlertsRssParser().parse(rssFeed)
-        val infoString = info.joinToString (separator = "\n\n") { it.toString() }
-        textView.text = infoString
+        /*val capAlert = assets.open("capAlert.xml")
+        val alert = CapParser().parse(capAlert)
+        val infoString = """
+            Identifier: ${alert.identifier}
+            MsgType: ${alert.msgType}
+            Sent: ${alert.sent}
+            Status: ${alert.status}
+            Info/event: ${alert.info.event}
+            Info/responseType: ${alert.info.responseType}
+            Info/instruction: ${alert.info.instruction}
+            Area/AreaDesc: ${alert.info.area.areaDesc}
+            Area/Polygon: ${alert.info.area.polygon?.substring(0,25)}...
+        """.trimIndent()
+        textView.text = infoString*/
     }
 }
