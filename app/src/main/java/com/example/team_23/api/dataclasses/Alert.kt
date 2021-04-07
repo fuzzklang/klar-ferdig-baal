14e897788d9cec725ece5e18f9c1b13f051cdc6b
Terje Sylvarnes <fuzzklang@gmail.com>
2021-04-07 13:38:55 +0200

    Legg til støtte for flere Info-elementer i Alert.

--- app/src/main/java/com/example/team_23/api/dataclasses/Alert.kt
+++ app/src/main/java/com/example/team_23/api/dataclasses/Alert.kt
@@ -5,9 +5,10 @@ package com.example.team_23.api.dataclasses
 // Contains only a selection of fields from CAP-alert
 // Need the HTTP-requests to properly work to be able to test these
 data class Alert (
-        val identifier: String?, // Unique identifier issued by MET or NVE
-        val sent: String?,       // Time of sending
-        val status: String?,     // 'Actual' or 'Test' (should be ignored)
-        val msgType: String?,    // 'Alert': first message. 'Update': replaces previous messages. 'Cancel': cancel previous erroneous message, or danger ended.
-        val info: Info
+        val identifier: String?, // Unik identifikator utstedt av MET eller NVE
+        val sent: String?,       // Tidspunkt for utsendelse
+        val status: String?,     // 'Actual' or 'Test' (test skal ignoreres)
+        val msgType: String?,    // 'Alert': første melding. 'Update': erstatter tidligere meldinger. 'Cancel': kanseller tidligere feilaktig melding, eller fare er over.
+        val infoItemsNo: List<Info>, // Liste med Info-elementer på norsk (vanligvis kun ett element?)
+        val infoItemsEn: List<Info>  // Liste med Info-elementer på engelsk (vanligvis kun ett element?)
 )
