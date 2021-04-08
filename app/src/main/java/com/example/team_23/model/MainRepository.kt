9dd8730ee239ed27f3ae6dbf3b660c06e5a2b7ed
sara <saradu@uio.no>
2021-04-08 16:25:35 +0200

    lagt til kartviewmodel

---
+++ app/src/main/java/com/example/team_23/model/MainRepository.kt
@@ -0,0 +1,5 @@
+package com.example.team_23.model
+
+class MainRepository {
+    //val apiService
+}
---
+++ app/src/main/java/com/example/team_23/viewmodel/kartViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.team_23.viewmodel
+
+class kartViewModel {
+
+
+
+   // fun visBaalplasser()
+
+    //fun hentVarsler()
+
+    //fun finnRuterFraTil()
+
+    //fun hentPos()
+
+   // fun hentVarselHeleKart()
+
+    // fun hentVarselForAngittRute()
+
+
+}
