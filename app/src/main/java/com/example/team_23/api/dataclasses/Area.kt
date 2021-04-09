4be53ee90454229b592a491c3078a5c344672b4a
Terje Sylvarnes <fuzzklang@gmail.com>
2021-04-09 12:29:09 +0200

    Erstatt gamle dataklasser med oppdaterte

--- app/src/main/java/com/example/team_23/api/dataclasses/Area.kt
+++
@@ -1,7 +0,0 @@
-package com.example.team_23.api.dataclasses
-
-// Area which alert is valid for
-data class Area (
-        val areaDesc: String? = null, // Description of geographical area for alert
-        val polygon: String? = null   // Polygon encircling the current area
-)
