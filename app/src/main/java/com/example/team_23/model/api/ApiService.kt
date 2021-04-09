5659834df45a9f269e44e2d9a35bf988eb9977a3
Terje Sylvarnes <fuzzklang@gmail.com>
2021-04-09 17:06:08 +0200

    Oppdater beskrivelse av abstrakt metode

--- app/src/main/java/com/example/team_23/model/api/ApiService.kt
+++ app/src/main/java/com/example/team_23/model/api/ApiService.kt
@@ -2,11 +2,13 @@ package com.example.team_23.model.api
 
 import androidx.annotation.WorkerThread
 
-
 interface ApiService {
-    // suspend: metoden gjør blokkerende kall (her nettverk)
-    // @WorkerThread-annoteringen gir beskjed til kompilatoren om at denne metoden ikke skal kalles
-    // på Main/UI-tråden. Hvis dette skulle skje gir den (forhåpentligvis) et varsel om dette.
+    /* suspend: metoden gjør blokkerende kall (her nettverk)
+    * @WorkerThread-annoteringen gir beskjed til kompilatoren om at denne metoden ikke skal kalles
+    * på Main/UI-tråden. Hvis dette skulle skje gir den (forhåpentligvis) et varsel om dette.
+    *
+    * param: endpoint endepunktet, options : http-request options
+    */
     @WorkerThread
     suspend fun fetchData(endpoint: String, options : List<String>): String
 }
