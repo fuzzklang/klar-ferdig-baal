481de471aa36585f495204f9f595a6bd8dd9e8f2
Terje Sylvarnes <fuzzklang@gmail.com>
2021-04-09 23:16:01 +0200

    Oppdater klassenavn, test ut API-kall

---
+++ app/src/main/java/com/example/team_23/viewmodel/KartViewModel.kt
@@ -0,0 +1,50 @@
+package com.example.team_23.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.example.team_23.model.MainRepository
+import com.example.team_23.model.api.dataclasses.Alert
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class KartViewModel {
+    // todo: Repo bør instansieres annetsteds, slik at det kun fins én av den!
+    val repo = MainRepository()
+    val varselListe = mutableListOf<Alert>()
+    val liveDataVarsler = MutableLiveData<MutableList<Alert>>()
+
+   // fun visBaalplasser()
+
+    fun hentVarsler() {
+        Log.d("kartViewModel", "Henter varsler!")
+        CoroutineScope(Dispatchers.IO).launch {
+            val rssItems = repo.getRssFeed()
+
+            // Dårlig idé med nøstede Coroutines? Men hvordan gjøre hver Alert-fetch asynkron?
+            CoroutineScope(Dispatchers.IO).launch {
+                rssItems?.forEach {
+                    val alert = repo.getCapAlert(it.link!!)
+                    if (alert != null) {
+                        varselListe.add(alert)
+                        liveDataVarsler.postValue(varselListe)
+                    }
+                }
+                varselListe.forEach {
+                    Log.d("viewmodel", "Alert: $it")
+                }
+            }
+        }
+    }
+
+    //fun finnRuterFraTil()
+
+    //fun hentPos()
+
+   // fun hentVarselHeleKart()
+
+    // fun hentVarselForAngittRute()
+
+
+}
