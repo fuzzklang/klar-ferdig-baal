113c966a6aa559f87052b2dc205794d99da0320a
Terje Sylvarnes <fuzzklang@gmail.com>
2021-04-09 23:18:54 +0200

    Implementer API-kall i ApiServiceImpl

--- app/src/main/java/com/example/team_23/model/api/ApiServiceImpl.kt
+++ app/src/main/java/com/example/team_23/model/api/ApiServiceImpl.kt
@@ -1,20 +1,51 @@
 package com.example.team_23.model.api
 
-import kotlinx.coroutines.delay
-import kotlin.random.Random
+import android.util.Log
+import okhttp3.*
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import java.io.IOException
 
 // Implementasjon av ApiService-grensesnittet
 class ApiServiceImpl: ApiService {
+    val tag = "ApiServiceImpl"
+    val client = OkHttpClient()
 
     /* Metode gjør http-request, returnerer en streng.
-     * @param url : String
+     * @param endpoint : String, options : liste av strenger som blir inkludert i request-url
      * @return String
      */
-    override suspend fun fetchData(endpoint: String, options : List<String>): String {
-        val httpResponse = ""
-        // Kall med OkHttp
-        // Try/catch?
-        // Returner streng
-        return httpResponse
+    @Throws(IOException::class)
+    override suspend fun fetchData(endpoint: String, options: List<String>): String? {
+        // Sårbart å lage URL selv, vurder å bruke OkHttp sin URL-Builder eller tilsvarende
+        val url = endpoint + "?" + options.joinToString("&")
+        // Kaller polymorf fetchData-metode. Retur-streng kan være null
+        return fetchData(url)
+    }
+
+    /* Metode lager en request-URL ved hjelp av OkHttp-klient og gjør nettverkskall.
+     * Returnerer streng eller null.
+     */
+    @Throws(IOException::class)
+    override suspend fun fetchData(url: String): String? {
+        // Bygg Request med OkHttp
+        val request = Request.Builder()
+            .url(url)
+            .build()
+        // Deklarer response
+        var response : Response
+        try {
+            // Gjør nettverkskall
+            response = client.newCall(request).execute()
+        } catch (ex: IOException) {
+            // Fang opp eventuell exception
+            Log.w(tag, "Feil under nettverkskall:\n$ex")
+            // Kast videre til håndtering
+            throw ex
+        }
+        val responseString = response.body?.string()
+        // Lukk Response-objektet
+        response.close()
+        // responseString kan være null
+        return responseString
     }
 }
