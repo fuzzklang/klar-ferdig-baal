363b62e09916216dc8d48faed1e13267ac3c70a1
Terje Sylvarnes <fuzzklang@gmail.com>
2021-04-12 11:17:28 +0200

    Sett opp kall på ViewModel i MainActivity for testing

--- app/src/main/java/com/example/team_23/view/MainActivity.kt
+++ app/src/main/java/com/example/team_23/view/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.team_23.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.team_23.R
+import com.example.team_23.viewmodel.KartViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel = KartViewModel()
+        viewModel.hentVarsler()
     }
 }
