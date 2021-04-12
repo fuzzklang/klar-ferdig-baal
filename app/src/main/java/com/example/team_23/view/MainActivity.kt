1c932d9e12d51da320fad4cb2f26f57650a72a9c
Terje Sylvarnes <fuzzklang@gmail.com>
2021-04-12 13:30:39 +0200

    Instansierer repository og gir videre til instanser av viewmodel-instanser

--- app/src/main/java/com/example/team_23/view/MainActivity.kt
+++ app/src/main/java/com/example/team_23/view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.team_23.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.team_23.R
+import com.example.team_23.model.MainRepository
 import com.example.team_23.viewmodel.KartViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewModel = KartViewModel()
-        viewModel.hentVarsler()
+        val repo = MainRepository()  // Flytt instansiering til Factory eller tilsvarende?
+        val viewModel = KartViewModel(repo)
+        viewModel.hentAlleVarsler()
     }
 }
