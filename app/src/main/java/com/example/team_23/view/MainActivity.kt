17cb5b2ff65dcc26d77c71707d5a7a66535b37b1
Terje Sylvarnes <fuzzklang@gmail.com>
2021-04-12 14:25:31 +0200

    Gjør API-kall ved button-trykk og viser varsler som tekst

--- app/src/main/java/com/example/team_23/view/MainActivity.kt
+++ app/src/main/java/com/example/team_23/view/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.team_23.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.example.team_23.R
 import com.example.team_23.model.MainRepository
 import com.example.team_23.viewmodel.KartViewModel
@@ -13,6 +15,25 @@ class MainActivity : AppCompatActivity() {
 
         val repo = MainRepository()  // Flytt instansiering til Factory eller tilsvarende?
         val viewModel = KartViewModel(repo)
-        viewModel.hentAlleVarsler()
+
+        val textView = findViewById<TextView>(R.id.myTextView)
+
+        viewModel.varsler.observe(this, { varselListe ->
+            var str = ""
+            varselListe.forEach {
+                textView.text = it.infoItemsNo[0].instruction
+                str +=  "${it.identifier}\n"
+                        "${it.msgType}\n" +
+                        "${it.infoItemsNo[0].event}\n" +
+                        "${it.infoItemsNo[0].instruction}\n\n"
+            }
+            textView.text = str
+        })
+
+        val btn = findViewById<Button>(R.id.btn)
+        btn.setOnClickListener {
+            textView.text = "Henter varsler"
+            viewModel.hentAlleVarsler()
+        }
     }
 }
