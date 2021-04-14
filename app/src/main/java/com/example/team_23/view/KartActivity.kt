4ca004d83d6df4b6d789256443e041677baeee68
sara <saradu@uio.no>
2021-04-14 18:47:28 +0200

    Lagt til regler knapp som sender bruker til activity_rules

--- app/src/main/java/com/example/team_23/view/KartActivity.kt
+++ app/src/main/java/com/example/team_23/view/KartActivity.kt
@@ -1,6 +1,8 @@
 package com.example.team_23.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.team_23.R
 import com.example.team_23.model.MainRepository
@@ -16,5 +18,14 @@ class KartActivity : AppCompatActivity() {
         val apiService = ApiServiceImpl()
         val repo = MainRepository(apiService)
         val kartViewModel = KartViewModel(repo)
+
+        //knapp som sender bruker til reglene
+        val rulesActicityBtn = findViewById<Button>(R.id.send_rules)
+
+        rulesActicityBtn.setOnClickListener{
+            val intent = Intent(this,RegelView::class.java)
+            startActivity(intent)
+
+        }
     }
 }
