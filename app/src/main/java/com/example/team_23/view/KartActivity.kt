83d28985f21cfc43a7818ba41ecb5597ed60b9a9
Terje Sylvarnes <fuzzklang@gmail.com>
2021-04-23 16:52:07 +0200

    Fjern ubrukte imports

--- app/src/main/java/com/example/team_23/view/KartActivity.kt
+++ app/src/main/java/com/example/team_23/view/KartActivity.kt
@@ -17,13 +17,6 @@ import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
 
-// import android.os.Bundle
-// import androidx.appcompat.app.AppCompatActivity
-// import com.example.team_23.R
-// import com.example.team_23.model.MainRepository
-// import com.example.team_23.model.api.ApiServiceImpl
-// import com.example.team_23.viewmodel.KartViewModel
-//
 class KartActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     val apiService = ApiServiceImpl()
