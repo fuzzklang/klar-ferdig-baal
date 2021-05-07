b61b0ee0b8276be59a95340f5c4061684e5d7e8d
Terje Sylvarnes <fuzzklang@gmail.com>
2021-05-07 13:54:57 +0200

    ViewModelProvider-test sjekker identitet, legg til kommentarer.
    Brukes assertSame for å sikre at testen sjekker identitet til kartViewModel-instans, og ikke bare at innholdet i klassen er likt. Ønsket oppførsel er at ViewModelProvider oppretter og holder på nøyaktig én instans av KartViewModel under kjøring av app.

--- app/src/androidTest/java/com/example/team_23/TestViewModelProvider.kt
+++ app/src/androidTest/java/com/example/team_23/TestViewModelProvider.kt
@@ -3,20 +3,38 @@ package com.example.team_23
 import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
 import com.example.team_23.utils.ViewModelProvider
+import com.example.team_23.viewmodel.KartViewModel
 import com.google.android.gms.location.LocationServices
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertSame
 import org.junit.Test
 
 class TestViewModelProvider {
     //val tag = "TestViewModelProvider"
     // appContext: Konteksten som appen testes under. Trengs for å aksessere assets-mappen
-    private val appContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
 
     @Test
     fun testSameInstanceReturnedOfKartViewModel() {
+        // Forventer at samme instans av kartViewModel returneres fra ViewModelProvider
+        // hver gang .getKartViewModel kalles
+        val appContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
         val fusedLocationProvider = LocationServices.getFusedLocationProviderClient(appContext)
-        val firstInstancekartViewModel = ViewModelProvider.getKartViewModel(fusedLocationProvider)
+        val firstInstanceKartViewModel = ViewModelProvider.getKartViewModel(fusedLocationProvider)
         val expectingIdenticalInstance = ViewModelProvider.getKartViewModel(fusedLocationProvider)
-        assertEquals(firstInstancekartViewModel, expectingIdenticalInstance)
+        assertSame(expectingIdenticalInstance, firstInstanceKartViewModel)
+    }
+
+    @Test
+    fun testSameInstanceReturnedOfKartViewModelRepeated() {
+        // Forventer at samme instans av kartViewModel returneres fra ViewModelProvider
+        // hver gang .getKartViewModel kalles. Repeterer kallene til ViewModelProvider
+        val appContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
+        val fusedLocationProvider = LocationServices.getFusedLocationProviderClient(appContext)
+        val firstInstanceKartViewModel = ViewModelProvider.getKartViewModel(fusedLocationProvider)
+        var expectingIdenticalInstance: KartViewModel = ViewModelProvider.getKartViewModel(fusedLocationProvider)
+        repeat(500) {
+            // Repeter kall på .getKartViewModel 500 ganger
+            expectingIdenticalInstance = ViewModelProvider.getKartViewModel(fusedLocationProvider)
+        }
+        assertSame(expectingIdenticalInstance, firstInstanceKartViewModel)
     }
 }
