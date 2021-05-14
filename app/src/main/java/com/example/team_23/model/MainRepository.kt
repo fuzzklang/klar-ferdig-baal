0144fc07a21c1d7e127affa22dd339a126b54996
selmahaugland <selmaeh@uio.no>
2021-05-14 11:38:14 +0200

    Lagde metode getPlaceFromLatLng som kaller på API-kallet fra Geocode

--- app/src/main/java/com/example/team_23/model/MainRepository.kt
+++ app/src/main/java/com/example/team_23/model/MainRepository.kt
@@ -43,10 +43,20 @@ class MainRepository(private val apiService: ApiServiceImpl, private val fusedLo
     //Geocode API
     private val placesURL = "https://maps.googleapis.com/maps/api/geocode/json?"
     private val key = "AIzaSyAyK0NkgPMxOOTnWR5EFKdy2DzfDXGh-HI"
+    private var placeName: List<String>? = null
 
     suspend fun getPlaceFromLatLng(latlng: LatLng) {
         Log.d(tag, "Soker etter sted fra Geocode API")
         val geocodePath = "${placesURL}${latlng}${key}"
+        try{
+            val httpResponse = apiService.fetchData(geocodePath)
+            if (httpResponse != null)  Log.d(tag, "Fikk respons fra Geocode API")
+            val response = gson.fromJson(httpResponse, Address_components::class.java)
+            placeName = response.types
+            Log.d("places", placeName.toString())
+        } catch (exception: IOException) {
+            Log.w(tag, "Feil under henting av types til sted: ${exception.message}")
+        }
     }
 
     suspend fun searchLocation(place: String): List<Candidates>?{
